6577d8226a1eb3be015164232ad49edb1d675fb1
Mike Hennessy <henworth@henabytes.com>
2026-02-03 23:54:05 -0500

    fix: Use ha-docker-socket-proxy directly

--- hawser/run.sh
+++ hawser/run.sh
@@ -2,19 +2,19 @@
 
 echo "[INFO] Hawser HA add-on starting in Standard mode"
 # echo "[INFO] Dockhand URL: ${dockhand_server_url}"
-echo "[DEBUG] $(ls -l /var/run)"
+# echo "[DEBUG] $(ls -l /var/run)"
 
-if [ ! -S /var/run/docker.sock ]; then
-  echo "[ERROR] Docker socket NOT found"
-  # ls -l /var/run || true
-  echo "[DEBUG] $(ls -l /var/run)"
-  exit 1
-fi
+# if [ ! -S /var/run/docker.sock ]; then
+#   echo "[ERROR] Docker socket NOT found"
+#   # ls -l /var/run || true
+#   echo "[DEBUG] $(ls -l /var/run)"
+#   exit 1
+# fi
 
-echo "[INFO] Docker socket found"
+# echo "[INFO] Docker socket found"
 
-# exec hawser \
-#   --log-level debug
+exec hawser \
+  --log-level debug
 
   # --server "${dockhand_server_url}" \
   # --token "${token}" \
