e02d833ae3e0afef565bbd97fa4b2ca4b7227517
Mike Hennessy <henworth@henabytes.com>
2026-02-03 23:29:51 -0500

    feat: Refactor based on ha-docker-socket-proxy
    Switched to Standard mode.

--- hawser/run.sh
+++ hawser/run.sh
@@ -1,17 +1,19 @@
 #!/usr/bin/with-contenv sh
 
-echo "[INFO] Hawser HA add-on starting"
-echo "[INFO] Dockhand URL: ${dockhand_server_url}"
+echo "[INFO] Hawser HA add-on starting in Standard mode"
+# echo "[INFO] Dockhand URL: ${dockhand_server_url}"
 
 if [ ! -S /var/run/docker.sock ]; then
   echo "[ERROR] Docker socket NOT found"
-  ls -l /var/run || true
+  # ls -l /var/run || true
+  echo "[DEBUG] $(ls -l /var/run)"
   exit 1
 fi
 
 echo "[INFO] Docker socket found"
 
 exec hawser \
-  --server "${dockhand_server_url}" \
-  --token "${token}" \
   --log-level debug
+
+  # --server "${dockhand_server_url}" \
+  # --token "${token}" \
